b372bb468bb40e1f95611a6a8e7ec5ccce5712f4
brokensythe <ade_1.0@live.co.uk>
2021-09-16 16:46:25 +0100

    created basic board

--- src/pages/GameBoardPage.jsx
+++ src/pages/GameBoardPage.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import "../styles/GameBoardPage.css"
 
-const INITIAL_BOARD = []
+const INITIAL_BOARD = [
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""],
+    ["","","","","","","",""]
+]
+
+const Container = styled.div`
+    height: 100vh;
+    display: grid;
+`
+
+const Board = styled.div`
+    display: grid;
+    grid-template-columns: repeat(8, minmax(30px, 60px));
+    grid-template-rows: repeat(8, minmax(30px, 60px));
+`
+
+const Square = styled.div`
+    background-color: ${props => (!(props.rank % 2) && !(props.file % 2)) || ((props.rank % 2) && (props.file % 2)) ? "black" : "white"};
+    color: aquamarine; 
+`
 
 function GameBoardPage() {
+
+    const [board, setBoard] = useState(INITIAL_BOARD)
+
     return (
-        <div className="Gameboard">
-            I exist
-        </div>
+        <Container>
+            <Board>
+                {
+                    board.map((rank, rankIndex) => rank.map((square, fileIndex) => {
+                        return <Square rank={rankIndex} file={fileIndex}>square</Square>
+                    }))
+                }
+            </Board>
+        </Container>
     )
 }
 
